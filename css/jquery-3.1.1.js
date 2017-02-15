bf847dac682902f4c07262b5474c4e5b7ff4c024
yumatezu <yuma.tezuka@internationalschool.ru>
2017-02-15 22:03:00 +0300

    Changed directory names

--- css/jquery-3.1.1.js
+++ css/jquery-3.1.1.js
@@ -1,32 +1,32 @@
 function passWord() {
 	var pass1 = prompt('Please Enter Your Password',' ');
 	if (!pass1) {
-		window.location.href = ('bad.html');
+		window.location.href = ('bad');
 	}
 	else if (pass1.toLowerCase() == "yep") {
-		window.location.href = ('webpage.html');
+		window.location.href = ('home');
 		return;
 	} 
 	else {
 		var pass2 = prompt('Password Incorrect','Password');
 		if (!pass2) {
-			window.location.href = ('bad.html');
+			window.location.href = ('/bad');
 		}
 		else if (pass2.toLowerCase() == "yep") {
-			window.location.href = ('webpage.html');
+			window.location.href = ('webpage');
 			return;
 		} 
 		else {
 			var pass3 = prompt('Incorrect, last try','Password');
 			if (!pass3) {
-				window.location.href = ('bad.html');
+				window.location.href = ('bad');
 			}
 			else if (pass3.toLowerCase() == "yep") {
-				window.location.href = ('webpage.html');
+				window.location.href = ('webpage');
 				return;
 			} 
 			else {
-				window.location.href = ('bad.html');
+				window.location.href = ('bad');
 			}
 		}
 	}
