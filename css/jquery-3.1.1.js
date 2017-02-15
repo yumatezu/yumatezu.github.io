5eb9de55d09ee4cf188051ae1308770d6338c2b9
yumatezu <yuma.tezuka@internationalschool.ru>
2017-02-15 21:34:17 +0300

    Bug Fixes

--- css/jquery-3.1.1.js
+++ css/jquery-3.1.1.js
@@ -1,31 +1,34 @@
 function passWord() {
-    var pass1 = prompt('Please Enter Your Password',' ');
-    if (!pass1) {
-        window.location.href = ('bad.html');
-    }
-    else if (pass1.toLowerCase() == "yep") {
-        window.location.href = ('webpage.html');
-    } 
-    else {
-        var pass2 = prompt('Password Incorrect','Password');
-        if (!pass2) {
-            window.location.href = ('bad.html');
-        }
-        else if (pass2.toLowerCase() == "yep") {
-            window.location.href = ('webpage.html');
-        } 
-        else {
-            var pass3 = prompt('Incorrect, last try','Password');
-            if (!pass3) {
-                window.location.href = ('bad.html');
-            }
-            else if (pass3.toLowerCase() == "yep") {
-                window.location.href = ('webpage.html');
-            } 
-            else {
-                window.location.href = ('bad.html');
-            }
-        }
-    }
+	var pass1 = prompt('Please Enter Your Password',' ');
+	if (!pass1) {
+		window.location.href = ('bad.html');
+	}
+	else if (pass1.toLowerCase() == "yep") {
+		window.location.href = ('webpage.html');
+		return;
+	} 
+	else {
+		var pass2 = prompt('Password Incorrect','Password');
+		if (!pass2) {
+			window.location.href = ('bad.html');
+		}
+		else if (pass2.toLowerCase() == "yep") {
+			window.location.href = ('webpage.html');
+			return;
+		} 
+		else {
+			var pass3 = prompt('Incorrect, last try','Password');
+			if (!pass3) {
+				window.location.href = ('bad.html');
+			}
+			else if (pass3.toLowerCase() == "yep") {
+				window.location.href = ('webpage.html');
+				return;
+			} 
+			else {
+				window.location.href = ('bad.html');
+			}
+		}
+	}
 }
 window.onload = passWord()
