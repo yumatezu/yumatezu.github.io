889879d8bce5264da7fbd103d528d27b28fa56dd
yumatezu <yuma.tezuka@internationalschool.ru>
2017-02-15 20:51:28 +0300

    Bug Fixes

--- css/jquery-3.1.1.js
+++ css/jquery-3.1.1.js
@@ -1,19 +1,21 @@
 function passWord() {
-var testV = 1;
 var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
 if (!pass1) 
-window.location.href = ('bad.html');
+  window.location.href = ('bad.html');
 if (pass1.toLowerCase() == "yep") {
   window.location.href = ('webpage.html');
   break;
 } else {
   var pass2 = prompt('Password Incorrect','Password');
+  if (!pass1) 
+    window.location.href = ('bad.html');
   if (pass2.toLowerCase() == "yep") {
     window.location.href = ('webpage.html');
     break;
   } else {
     var pass3 = prompt('Incorrect, last try','Password');
+    if (!pass1) 
+     window.location.href = ('bad.html');
     if (pass3.toLowerCase() == "yep") {
       window.location.href = ('webpage.html');
       break;
